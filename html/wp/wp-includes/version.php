19fb0465335087faa09e4d86fc25a37c51a0b7e0
William Yao <abpwill@gmail.com>
2016-05-03 17:37:26 +0800

    Upgrade wordpress to latest version 4.5.1

--- html/wp/wp-includes/version.php
+++ html/wp/wp-includes/version.php
@@ -4,21 +4,21 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.0.10';
+$wp_version = '4.5.1';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
  *
  * @global int $wp_db_version
  */
-$wp_db_version = 29632;
+$wp_db_version = 36686;
 
 /**
  * Holds the TinyMCE version
  *
  * @global string $tinymce_version
  */
-$tinymce_version = '4104-20150505';
+$tinymce_version = '4310-20160418';
 
 /**
  * Holds the required PHP version
