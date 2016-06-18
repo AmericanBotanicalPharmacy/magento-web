0283302a0637ce4657964f986e0ec27fffdcbdaf
William Yao <abpwill@gmail.com>
2016-06-18 12:27:08 +0800

    Create MailChimp module.

---
+++ html/app/code/local/Abp/MailChimp/Helper/Data.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Abp_Mailchimp helper
+ *
+ * @category    Abp
+ * @package     Abp_Mailchimp
+ * @author      Edward Yang <yushine999@gmail.com>
+ */
+class Abp_Mailchimp_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
