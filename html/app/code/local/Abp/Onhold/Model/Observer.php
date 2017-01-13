622bc5c8c5dfb9bf5fe53b9fbb4c8bc2d770644f
wei.shen <shenwei235@gmail.com>
2017-01-13 21:16:41 +0800

    Re#605 bind observer to checkout_onepage_controller_success_action event.

--- html/app/code/local/Abp/Onhold/Model/Observer.php
+++ html/app/code/local/Abp/Onhold/Model/Observer.php
@@ -1,25 +1,32 @@
 <?php
 
 class Abp_Onhold_Model_Observer
-{
-    public function holdOrder($event)
+{   
+    public function holdOrder(Varien_Event_Observer $observer)
     {
-        $order = $event->getOrder();
-        $total = $order->getGrandTotal();
-        $s_zip = $order->getShippingAddress()->getData('postcode');
-        $b_zip = $order->getBillingAddress()->getData('postcode');
-        $s_region_id = $order->getShippingAddress()->getData('region_id');
-        $enable = Mage::getStoreConfig('onhold/settings/active', Mage::app()->getStore());
-        $amount = intval(Mage::getStoreConfig('onhold/settings/amount', Mage::app()->getStore()));
-        $states = Mage::getStoreConfig('onhold/settings/specificstates', Mage::app()->getStore());
-        $states_arr = explode(",", $states);
-        // Abp_Onhold module is active.
-        // shipping address zip is NOT same as billing address zip
-        // shipping address region is IN onhold/settings/specificstates.
-        // order's grand total is greater than onhold/settings/amount.
-        // then change order's status to ON HOLD.
-        if($enable == '1' && $s_zip != $b_zip && in_array($s_region_id, $states_arr) && $total > $amount) {
-            $this->_processOrderStatus($order);
+        $orderIds = $observer->getEvent()->getOrderIds();
+        if (empty($orderIds) || !is_array($orderIds)) {
+            return;
+        }
+        // loop order ids, as normal there's ONLY one order id.
+        foreach($orderIds as $value) {
+            $order = Mage::getModel('sales/order')->load($value);
+            $total = $order->getGrandTotal();
+            $s_zip = $order->getShippingAddress()->getData('postcode');
+            $b_zip = $order->getBillingAddress()->getData('postcode');
+            $s_region_id = $order->getShippingAddress()->getData('region_id');
+            $enable = Mage::getStoreConfig('onhold/settings/active', Mage::app()->getStore());
+            $amount = intval(Mage::getStoreConfig('onhold/settings/amount', Mage::app()->getStore()));
+            $states = Mage::getStoreConfig('onhold/settings/specificstates', Mage::app()->getStore());
+            $states_arr = explode(",", $states);
+            // Abp_Onhold module is active.
+            // shipping address zip is NOT same as billing address zip
+            // shipping address region is IN onhold/settings/specificstates.
+            // order's grand total is greater than onhold/settings/amount.
+            // then change order's status to ON HOLD.
+            if($enable == '1' && $s_zip != $b_zip && in_array($s_region_id, $states_arr) && $total > $amount) {
+                $this->_processOrderStatus($order);
+            }
         }
         return $this;
     }
@@ -36,7 +43,7 @@ class Abp_Onhold_Model_Observer
         $order->addStatusHistoryComment($statusMessage);
         // do NOT notify customer...
         $order->setCustomerNoteNotify(false);
-        $order->setState(Mage_Sales_Model_Order::STATE_HOLDED, true);       
+        $order->hold();       
         $order->save();
     }
 }
