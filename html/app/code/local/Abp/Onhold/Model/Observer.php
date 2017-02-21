75081f2944861156bcb00b254aebafe69d325b67
wei.shen <shenwei235@gmail.com>
2017-02-21 18:52:25 +0800

    Re#605 validate shipping address country_id in criteria as well, only US can pass

--- html/app/code/local/Abp/Onhold/Model/Observer.php
+++ html/app/code/local/Abp/Onhold/Model/Observer.php
@@ -13,6 +13,7 @@ class Abp_Onhold_Model_Observer
             $order = Mage::getModel('sales/order')->load($value);
             $total = $order->getGrandTotal();
             $s_zip = $order->getShippingAddress()->getData('postcode');
+            $country_id = $order->getShippingAddress()->getData('country_id');
             $b_zip = $order->getBillingAddress()->getData('postcode');
             $s_region_id = $order->getShippingAddress()->getData('region_id');
             $enable = Mage::getStoreConfig('onhold/settings/active', Mage::app()->getStore());
@@ -21,10 +22,11 @@ class Abp_Onhold_Model_Observer
             $states_arr = explode(",", $states);
             // Abp_Onhold module is active.
             // shipping address zip is NOT same as billing address zip
+            // shipping address country_id is US.
             // shipping address region is IN onhold/settings/specificstates.
             // order's grand total is greater than onhold/settings/amount.
             // then change order's status to ON HOLD.
-            if($enable == '1' && $s_zip != $b_zip && in_array($s_region_id, $states_arr) && $total > $amount) {
+            if($enable == '1' && $s_zip != $b_zip && $country_id == 'US' && in_array($s_region_id, $states_arr) && $total > $amount) {
                 $this->_processOrderStatus($order);
             }
         }
